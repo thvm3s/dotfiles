767cde015eb94aa58d8f903a2c12bd7599548989
Thames Sookpranee <thvm3s@gmail.com>
2023-01-08 20:56:22 +0700

    Set hemisu as colorscheme

--- nvim/lua/thvm3s/plugins.lua
+++ nvim/lua/thvm3s/plugins.lua
@@ -12,4 +12,6 @@ packer.startup(function(use)
 	use 'nvim-lua/plenary.nvim'
   use 'nvim-telescope/telescope.nvim'
   use 'nvim-telescope/telescope-file-browser.nvim'
+  use 'noahfrederick/vim-hemisu'
 end)
+
